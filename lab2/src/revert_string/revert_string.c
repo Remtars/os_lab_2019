cb75c8df1454819f396896074991b8f4d945d7ae
remtars <rusakov.artemy@gmail.com>
2020-03-21 12:32:50 +0000

    added swapper and reverter

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,22 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    char *p = str;
+    int n = 0;
+    
+    while (*p != '\0')
+    {
+        n++;
+        p++;
+    }
+
+    char buff;
+
+    for (int i=0; i<n/2; i++)
+    {
+        buff = str[n-i-1];
+        str[n-i-1] = str[i];
+        str[i] = buff;
+    }
 }
 
